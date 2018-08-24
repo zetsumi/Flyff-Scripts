a74b252ae722d04f3524437f845124308fbf8174
Cyril Louisin <cyril.louisin@outlook.fr>
2018-08-24 11:09:59 +0200

    load  [WORLD].wld

--- world.py
+++ world.py
@@ -3,6 +3,7 @@ import os
 from collections import OrderedDict
 from logger import gLogger
 from text import Text
+from common import Vector
 
 file_listing_world = "Ressource/World.inc"
 
@@ -13,6 +14,20 @@ class World:
         self.title= str()
         self.directory = str()
         self.text = Text()
+        self.size = Vector(0,0,0)
+        self.indoor = int()
+        self.ambient = str() #hexa
+        self.bgColor = str() #hexa
+        self.fly = int()
+        self.camera = list()
+        self.revival = list()
+        self.diffuse = str()
+        self.lightDir = list()
+        self.fogSetting = list()
+        self.bgm = list()
+        self.pkmode = list()
+        self.MPU = 4
+
 
 
 class Worlds:
@@ -52,7 +67,7 @@ class Worlds:
                     world = World()
                     world.id = str(arr[0])
                     if world.id not in defineWorld:
-                        gLogger.error("World undeclared:", world.id)
+                        gLogger.error("World undeclared: [{id_world}]".format(id_world=world.id))
                         continue
                     world.directory = str(arr[1]).replace("\"", "")
                     self.worlds[world.id] = world
@@ -65,15 +80,36 @@ class Worlds:
                         gLogger.error("SetTitle on world undeclared:", index)
 
 
+    def __load_wld__(self, f):
+        with open(f, "r") as fd:
+            for line in fd:
+                line = line.replace("\n", "")
+                if "\t" not in line and " " in line:
+                    arr = line.split(" ")
+                elif " " not in line and "\t" in line:
+                    arr = line.split("\t")
+                elif " " in line and "\t" in line:
+                    line = line.replace("\t", "")
+                    arr = line.split(" ")
+                if "//" in arr[0]:
+                    continue
+                if arr[0] == "size":
+                    arr[1] = arr[1].replace(",", "")
+                    arr[2] = arr[1].replace(",", "")
+                    self.size = Vector(int(arr[1], 10), int(arr[2], 10), 0)
+                elif arr[0] == "indoor":
+                    self.indoor = int(arr[1])
+                elif arr[0] == "MPU":
+                    self.MPU = int(arr[1])
+
+
     def load(self, path_world, defineWorld):
         gLogger.set_section("world")
 
         self.__load_world_inc__(defineWorld)
         for it in self.worlds:
             world = self.worlds[it]
-            if os.path.exists(path_world + world.directory) is False:
-                gLogger.error("Path does not exists:", path_world + world.directory)
-                continue
             world.text.load(path_world + world.directory + "/" + world.directory + ".txt.txt")
+            self.__load_wld__(path_world + world.directory + "/" + world.directory + ".wld")
 
         gLogger.reset_section()
